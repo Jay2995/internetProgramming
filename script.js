e708dccaa4ea38455e70c83014afa9410520931b
Jay2995 <josey02995@gmail.com>
2022-11-18 18:07:17 +0300

    classes and ids

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict'
+
+document.getElementById("changeThis1").textContent = 1;
+document.getElementById("changeThis2").textContent = 2;
+document.getElementById("changeThis3").textContent = 3;
+document.getElementById("changeThis4").textContent = 4;
+
