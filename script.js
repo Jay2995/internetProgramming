389967aa86db6486d24dc590f5fd0c4c9fab9daa
Jay2995 <josey02995@gmail.com>
2022-11-23 16:22:31 +0300

    grid

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ document.getElementById("changeThis2").textContent = 2;
 document.getElementById("changeThis3").textContent = 3;
 document.getElementById("changeThis4").textContent = 4;
 
+
+//container 
+
+document.querySelectorAll(".grid-container").forEach(element => 
+   element.style.backgroundColor = "rgb(8,100,100)")
+
+
+// console.log(document.querySelectorAll(".grid-container"));
