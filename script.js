62563fcdf3619b8f27687c1d1b8a0d3125ea42f4
Jay2995 <josey02995@gmail.com>
2022-12-14 16:10:14 +0300

    feedback form
    .

--- script.js
+++ script.js
@@ -12,4 +12,29 @@ document.querySelectorAll(".rows").forEach(element =>
    element.style.backgroundColor = "rgb(8,100,100)")
 
 
-// console.log(document.querySelectorAll(".grid-container"));
+// console.log(document.querySelectorAll(".rows"));
+const feedbackCommentByUser = document.querySelector('.comment')
+// console.log(feedbackCommentByUser);
+const sendButton = document.querySelector('input[type=submit]')
+const resetBtn = document.querySelector('input[type=reset]')
+const feedback = document.getElementById('feedbackReturn')
+
+// console.log(feedback);
+
+
+
+sendButton.addEventListener('click', function(e){
+   e.preventDefault()
+   console.log();
+   if(!feedbackCommentByUser.value)alert('No comment')
+      feedback.insertAdjacentHTML('beforeend', feedbackCommentByUser.value)
+      feedbackCommentByUser.value =''
+
+})
+   resetBtn.addEventListener('click', function(e){
+   e.preventDefault()
+   feedbackCommentByUser.value = feedback.textContent = ''
+   
+   })
+// console.log(feedbackCommentByUser);
+
